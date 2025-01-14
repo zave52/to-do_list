94457bee89fee0e4c1468e593039326ffdd9aa5e
zakhar <zaharsavchen@gmail.com>
2025-01-15 00:33:33 +0200

    create views, models and urls

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline_datetime = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False, blank=True)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ("is_completed", "-datetime",)
+
+    def __str__(self) -> str:
+        return (
+            f"{self.content} "
+            f"({', '.join([str(tag) for tag in self.tags.all()])})"
+        )
--- app/views.py
+++ app/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.urls.base import reverse
+from django.views import generic
 
-# Create your views here.
+from app.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 10
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = ("content", "deadline_datetime", "tags",)
+    success_url = reverse_lazy("app:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = ("content", "deadline_datetime", "is_completed", "tags",)
+    success_url = reverse_lazy("app:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("app:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 10
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("app:tag-list")
+
+
+def toggle_complete_status(request: HttpRequest, pk: int):
+    task = Task.objects.get(pk=pk)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect(reverse("app:task-list"))
